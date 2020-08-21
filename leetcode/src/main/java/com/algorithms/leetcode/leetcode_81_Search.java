47eabc7bf124aa46494df8aa7d1fdede41dbcc42
zxj <zhanxuejun@unisound.com>
2020-08-21 16:04:38 +0800

    增加leetcode 删除重复数字的链表

--- leetcode/src/main/java/com/algorithms/leetcode/leetcode_81_Search.java
+++ leetcode/src/main/java/com/algorithms/leetcode/leetcode_81_Search.java
@@ -5,7 +5,7 @@ package com.algorithms.leetcode;
  * ( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。
  * 编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。
  */
-public class leetcode_81_search {
+public class leetcode_81_Search {
 
     public static boolean search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
