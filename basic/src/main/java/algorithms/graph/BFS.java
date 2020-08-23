cafc57509c86defde0880e7658589c6e7f704fef
zxj <zhanxuejun@unisound.com>
2020-08-23 20:53:45 +0800

    增加leetcode 计算柱状图的最大面积以及图的算法

--- basic/src/main/java/algorithms/graph/BFS.java
+++ basic/src/main/java/algorithms/graph/BFS.java
@@ -1,7 +1,31 @@
 package algorithms.graph;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFS {
 
     // 从node出发，进行宽度优先遍历
+    public static void bfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> set = new HashSet<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
 
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    set.add(next);
+                    queue.add(next);
+                }
+            }
+        }
+
+
+    }
 }
---
+++ basic/src/main/java/algorithms/graph/Dijkstra.java
@@ -0,0 +1,54 @@
+package algorithms.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Dijkstra {
+
+    public static HashMap<Node, Integer> dijkstra1(Node from) {
+        // 从head出发到所有点的最小距离
+        // key : 从head出发到达key
+        // value : 从head出发到达key的最小距离
+        // 如果在表中，没有T的记录，含义是从head出发到T这个点的距离为正无穷
+        HashMap<Node, Integer> distanceMap = new HashMap<>();
+        distanceMap.put(from, 0);
+        // 已经求过距离的节点，存在selectedNodes中，以后再也不碰
+        HashSet<Node> selectedNodes = new HashSet<>();
+        // from 0
+        Node minNode = getMinDistanceAndUnselectedNode(distanceMap, selectedNodes);
+
+        while (minNode != null) {
+            int distance = distanceMap.get(minNode);
+            for (Edge edge : minNode.edges) {
+                Node toNode = edge.to;
+                if (!distanceMap.containsKey(toNode)) {
+                    distanceMap.put(toNode, distance + edge.weight);
+                } else {
+                    distanceMap.put(edge.to, Math.min(distanceMap.get(toNode), distance + edge.weight));
+                }
+            }
+            selectedNodes.add(minNode);
+            minNode = getMinDistanceAndUnselectedNode(distanceMap, selectedNodes);
+        }
+        return distanceMap;
+    }
+
+
+    public static Node getMinDistanceAndUnselectedNode(HashMap<Node, Integer> distanceMap,
+                                                       HashSet<Node> touchedNodes) {
+        Node minNode = null;
+        int minDistance = Integer.MAX_VALUE;
+
+        for (Map.Entry<Node, Integer> entry : distanceMap.entrySet()) {
+            Node node = entry.getKey();
+            int distance = entry.getValue();
+
+            if (!touchedNodes.contains(node) && distance < minDistance) {
+                minNode = node;
+                minDistance = distance;
+            }
+        }
+        return minNode;
+    }
+}
