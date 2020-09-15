9516fc133e54fe8bb06155cde52a61a530d3295c
zxj <zhanxuejun@unisound.com>
2020-09-15 21:55:38 +0800

    增加leetcode 规则接口开发

---
+++ leetcode/src/main/java/com/algorithms/leetcode/onehundred/leetcode_16_ThreeSumClosest.java
@@ -0,0 +1,33 @@
+package com.algorithms.leetcode.onehundred;
+
+
+public class leetcode_16_ThreeSumClosest {
+
+    public static int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3)
+            return 0;
+
+        int l = 0;
+        int r = nums.length - 1;
+        int closet = Integer.MAX_VALUE;
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+
+                    if (Math.abs(nums[i] + nums[j] + nums[k] - target) < closet) {
+                        closet = Math.abs(nums[i] + nums[j] + nums[k] - target);
+                        ans = nums[i] + nums[j] + nums[k];
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-55, -24, -18, -11, -7, -3, 4, 5, 6, 9, 11, 23, 33};
+        System.out.println(threeSumClosest(arr, 0));
+    }
+}
---
+++ leetcode/src/main/java/com/algorithms/leetcode/onehundred/leetcode_18_FourSum.java
@@ -0,0 +1,72 @@
+package com.algorithms.leetcode.onehundred;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class leetcode_18_FourSum {
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums == null || nums.length < 3) return new ArrayList<>();
+        //首先排序
+        Arrays.sort(nums);
+
+        List<List<Integer>> result = new ArrayList<>();
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            int tempTarget = target - nums[i];
+            if (i > 0 && i <= len - 3 && nums[i] == nums[i - 1]) continue; // 去重
+            for (int j = i + 1; j < len; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    j++;
+                } // 去重
+                int l = j + 1;
+                int r = nums.length - 1;
+                while (l < r) {
+                    if ((nums[l] + nums[j] + nums[r]) > tempTarget) {
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        r--;
+                    } else if ((nums[l] + nums[j] + nums[r]) < tempTarget) {
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        l++;
+                    } else {
+                        //等于0
+                        List<Integer> ans = new ArrayList<>();
+                        ans.add(nums[i]);
+                        ans.add(nums[j]);
+                        ans.add(nums[l]);
+                        ans.add(nums[r]);
+                        result.add(ans);
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        while (r > l && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    }
+                }
+
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{-1, 0, 1, 2, -1, -4};
+        int target = -1;
+
+        List<List<Integer>> list = fourSum(arr, target);
+
+        for (List<Integer> list1 : list) {
+            for (Integer i : list1) {
+                System.out.printf("%s\t", i);
+            }
+            System.out.println();
+        }
+    }
+
+}
