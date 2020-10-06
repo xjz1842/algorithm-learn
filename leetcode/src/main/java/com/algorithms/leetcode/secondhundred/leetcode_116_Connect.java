e792ecec79935b9c0b21783f47a024b94c94ed67
zxj <zhanxuejun@unisound.com>
2020-10-06 22:40:27 +0800

    增加leetcode 股票最大收益问题

---
+++ leetcode/src/main/java/com/algorithms/leetcode/secondhundred/leetcode_116_Connect.java
@@ -0,0 +1,58 @@
+package com.algorithms.leetcode.secondhundred;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class leetcode_116_Connect {
+
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        if (root == null)
+            return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            //每一层的数量
+            int levelCount = queue.size();
+            //前一个节点
+            Node pre = null;
+            for (int i = 0; i < levelCount; i++) {
+                //出队
+                Node node = queue.poll();
+                //如果pre为空就表示node节点是这一行的第一个，
+                //没有前一个节点指向他，否则就让前一个节点指向他
+                if (pre != null) {
+                    pre.next = node;
+                }
+                //然后再让当前节点成为前一个节点
+                pre = node;
+                //左右子节点如果不为空就入队
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+        }
+        return root;
+    }
+}
--- leetcode/src/main/java/com/algorithms/leetcode/secondhundred/leetcode_124_MaxPathSum.java
+++ leetcode/src/main/java/com/algorithms/leetcode/secondhundred/leetcode_124_MaxPathSum.java
@@ -36,6 +36,58 @@ public class leetcode_124_MaxPathSum {
         return Math.max(leftGrain, rightGrain) + root.val;
     }
 
+
+    public static class Info {
+        public int maxPathSum;
+        public int maxPathSumFromHead;
+
+        public Info(int path, int head) {
+            maxPathSum = path;
+            maxPathSumFromHead = head;
+        }
+    }
+
+    public static int maxPathSum1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return process1(root).maxPathSum;
+    }
+
+    public static Info process1(TreeNode x) {
+        if (x == null) {
+            return null;
+        }
+        Info leftInfo = process1(x.left);
+        Info rightInfo = process1(x.right);
+        int p1 = Integer.MIN_VALUE;
+        if (leftInfo != null) {
+            p1 = leftInfo.maxPathSum;
+        }
+        int p2 = Integer.MIN_VALUE;
+        if (rightInfo != null) {
+            p2 = rightInfo.maxPathSum;
+        }
+        int p3 = x.val;
+        int p4 = Integer.MIN_VALUE;
+        if (leftInfo != null) {
+            p4 = x.val + leftInfo.maxPathSumFromHead;
+        }
+        int p5 = Integer.MIN_VALUE;
+        if (rightInfo != null) {
+            p5 = x.val + rightInfo.maxPathSumFromHead;
+        }
+        int p6 = Integer.MIN_VALUE;
+        if (leftInfo != null && rightInfo != null) {
+            p6 = x.val + leftInfo.maxPathSumFromHead + rightInfo.maxPathSumFromHead;
+        }
+        int maxSum = Math.max(Math.max(Math.max(p1, p2), Math.max(p3, p4)), Math.max(p5, p6));
+        int maxSumFromHead = Math.max(p3, Math.max(p4, p5));
+
+        return new Info(maxSum, maxSumFromHead);
+    }
+
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(2);
         root.left = new TreeNode(-1);
