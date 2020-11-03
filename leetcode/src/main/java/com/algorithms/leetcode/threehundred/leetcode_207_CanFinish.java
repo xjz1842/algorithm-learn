b9e9ff057f46d2d972eded31a5f036564d63bc83
zxj <zhanxuejun@unisound.com>
2020-11-03 21:43:43 +0800

    增加leetcode  课程表

---
+++ leetcode/src/main/java/com/algorithms/leetcode/threehundred/leetcode_207_CanFinish.java
@@ -0,0 +1,35 @@
+package com.algorithms.leetcode.threehundred;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class leetcode_207_CanFinish {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegrees = new int[numCourses];
+        List<List<Integer>> adjacency = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++)
+            adjacency.add(new ArrayList<>());
+        // Get the indegree and adjacency of every course.
+        for (int[] cp : prerequisites) {
+            indegrees[cp[0]]++;
+            adjacency.get(cp[1]).add(cp[0]);
+        }
+        // Get all the courses with the indegree of 0.
+        for (int i = 0; i < numCourses; i++)
+            if (indegrees[i] == 0) queue.add(i);
+        // BFS TopSort.
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            for (int cur : adjacency.get(pre))
+                if (--indegrees[cur] == 0) queue.add(cur);
+        }
+        return numCourses == 0;
+    }
+}
+
+
