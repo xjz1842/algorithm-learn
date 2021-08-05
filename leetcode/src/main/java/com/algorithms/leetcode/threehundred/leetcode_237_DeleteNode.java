38104c6068a065b1660263aed76781f3b9c383b1
zhanxuejun <905159@zxj>
2021-08-05 11:12:12 +0800

    leetcode之删除节点

---
+++ leetcode/src/main/java/com/algorithms/leetcode/threehundred/leetcode_237_DeleteNode.java
@@ -0,0 +1,34 @@
+package com.algorithms.leetcode.threehundred;
+
+public class leetcode_237_DeleteNode {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+        public static void deleteNode(ListNode node) {
+            if(node == null)
+                return;
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        deleteNode(head.next.next);
+
+        while (head != null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
