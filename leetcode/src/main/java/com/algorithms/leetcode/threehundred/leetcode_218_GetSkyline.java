0cbf8d1f903210996291c81b01726e1bd841a5f1
zhanxuejun <905159@zxj>
2021-08-13 14:05:07 +0800

    leetcode之天际线问题

---
+++ leetcode/src/main/java/com/algorithms/leetcode/threehundred/leetcode_218_GetSkyline.java
@@ -0,0 +1,81 @@
+package com.algorithms.leetcode.threehundred;
+
+import java.util.*;
+
+public class leetcode_218_GetSkyline {
+
+
+    public static List<List<Integer>> getSkyline(int[][] buildings) {
+        if (buildings == null || buildings.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        List<int[]> ps = new ArrayList<>();
+        for (int[] b : buildings) {
+            int l = b[0], r = b[1], h = b[2];
+            ps.add(new int[]{l, h, -1});
+            ps.add(new int[]{r, h, 1});
+        }
+        /**
+         * 先严格按照横坐标进行「从小到大」排序
+         * 对于某个横坐标而言，可能会同时出现多个点，应当按照如下规则进行处理：
+         * 1. 优先处理左端点，再处理右端点
+         * 2. 如果同样都是左端点，则按照高度「从大到小」进行处理（将高度增加到优先队列中）
+         * 3. 如果同样都是右端点，则按照高度「从小到大」进行处理（将高度从优先队列中删掉）
+         */
+        ps.sort((a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            if (a[2] != b[2]) return a[2] - b[2];
+            if (a[2] == -1) {
+                return b[1] - a[1];
+            } else {
+                return a[1] - b[1];
+            }
+        });
+        // 记录进行了删除操作的高度，以及删除次数
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> q = new PriorityQueue<>((a,b)->b-a);
+        int prev = 0;
+        q.add(prev);
+        for (int[] p : ps) {
+            int point = p[0], height = p[1], flag = p[2];
+            if (flag == -1) {
+                q.add(height);
+            } else {
+                map.put(height, map.getOrDefault(height, 0) + 1);
+            }
+
+            while (!q.isEmpty()) {
+                int peek = q.peek();
+                if (map.containsKey(peek)) {
+                    if (map.get(peek) == 1) map.remove(peek);
+                    else map.put(peek, map.get(peek) - 1);
+                    q.poll();
+                } else {
+                    break;
+                }
+            }
+
+            int cur = q.peek();
+            if (cur != prev) {
+                List<Integer> list = new ArrayList<>();
+                list.add(point);
+                list.add(cur);
+                ans.add(list);
+                prev = cur;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] buidings = new int[][]{
+                {2, 9, 10},
+                {3, 7, 15},
+                {5, 12, 12},
+                {15, 20, 10},
+                {19, 24, 8}
+        };
+        System.out.println(getSkyline(buidings));
+    }
+}
