dc765c4633969f13945548740ad3695b55765e34
zhanxuejun <905159@zxj>
2021-08-31 09:38:48 +0800

    leetcode  二叉搜索树中第K小的元素

---
+++ leetcode/src/main/java/com/algorithms/leetcode/threehundred/leetcode_230_kthSmallest.java
@@ -0,0 +1,53 @@
+package com.algorithms.leetcode.threehundred;
+
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class leetcode_230_kthSmallest {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int kthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while (true) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) return root.val;
+            root = root.right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.left.right = new TreeNode(2);
+        int k = 1;
+        System.out.println(kthSmallest(root, k));
+    }
+}
