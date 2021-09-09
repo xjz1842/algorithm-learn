20323a5b082c28de1b79c70b3735d66c64ee2c2a
zhanxuejun <905159@zxj>
2021-09-09 16:56:46 +0800

    leetcode 区域和检索 - 数组不可变

---
+++ leetcode/src/main/java/com/algorithms/leetcode/fourhundred/leetcode_303_NumArray.java
@@ -0,0 +1,27 @@
+package com.algorithms.leetcode.fourhundred;
+
+public class leetcode_303_NumArray {
+
+    private static long[] preproessArr;
+
+    public leetcode_303_NumArray(int[] nums) {
+        preproessArr = new long[nums.length];
+        preproessArr[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            preproessArr[i] = preproessArr[i - 1] + nums[i];
+        }
+    }
+
+    public static int sumRange(int left, int right) {
+        if (left == 0) {
+            return (int) preproessArr[right];
+        }
+        return (int) (preproessArr[right] - preproessArr[left - 1]);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 0, 3, -5, 2, -1};
+        leetcode_303_NumArray numArray = new leetcode_303_NumArray(nums);
+        System.out.println(sumRange(0, 2));
+    }
+}
