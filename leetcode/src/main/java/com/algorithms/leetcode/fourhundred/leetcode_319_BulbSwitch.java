9c1bbc7f16909aa431f7ce6659b9acb2f8902c81
zhanxuejun <905159@zxj>
2021-09-21 14:04:00 +0800

    优化

--- leetcode/src/main/java/com/algorithms/leetcode/fourhundred/leetcode_319_BulbSwitch.java
+++ leetcode/src/main/java/com/algorithms/leetcode/fourhundred/leetcode_319_BulbSwitch.java
@@ -1,17 +1,18 @@
 package com.algorithms.leetcode.fourhundred;
 
+
 public class leetcode_319_BulbSwitch {
 
        public static int bulbSwitch(int n) {
             int count = 0; 
-            for(int i = 1; i * i <= n; i++){
+            for(int i = 1; i * i < n; i++){
                 count++;
             }
             return count;
        }
 
        public static void main(String[] args) {
-           int n = 8;
+           int n = 1;
            System.out.println(bulbSwitch(n));
        }
 }
