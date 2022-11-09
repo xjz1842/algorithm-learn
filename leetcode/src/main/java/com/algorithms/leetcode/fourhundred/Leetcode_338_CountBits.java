c39d93469b2e703694c2509c575446df9b87fcb4
zxj <zhanxuejun@unisound.com>
2022-11-09 08:09:55 +0800

    计算二进制的个数

--- leetcode/src/main/java/com/algorithms/leetcode/fourhundred/Leetcode_338_CountBits.java
+++ leetcode/src/main/java/com/algorithms/leetcode/fourhundred/Leetcode_338_CountBits.java
@@ -18,10 +18,22 @@ public class Leetcode_338_CountBits {
         return ans;
     }
 
+    //动态规划
+    public static int[] countBits1(int n) {
+        int[] dp = new int[n + 1];
+        //初始化
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i >> 1] + ((i & 1) == 1 ? 1 : 0);
+        }
+        return dp;
+    }
+
     public static void main(String[] args) {
-       int n = 5;
-       for(int i : countBits(n)){
-          System.out.println(i);
-      }
+        int n = 5;
+        for (int i : countBits(n)) {
+            System.out.println(i);
+        }
     }
 }
